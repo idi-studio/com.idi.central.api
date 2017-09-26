168f3f1742469fd8134ebce4db37527d46317c08
johnnywang <johnny_l_wang@manulife.com>
2017-09-26 10:38:07 +0800

    Initial

---
+++ src/IDI.Core/Authentication/IPermission.cs
@@ -0,0 +1,13 @@
+﻿using IDI.Core.Common.Enums;
+
+namespace IDI.Core.Authentication
+{
+    public interface IPermission
+    {
+        string Name { get;  }
+
+        string Code { get; }
+
+        PermissionType Type { get; }
+    }
+}
