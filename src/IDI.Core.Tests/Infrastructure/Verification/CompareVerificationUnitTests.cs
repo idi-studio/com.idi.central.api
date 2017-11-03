b4ab0adb7218ff835d3344ba1010ecead3b3c396
johnnywang <johnny_l_wang@manulife.com>
2017-11-03 17:39:02 +0800

    Initial

--- src/IDI.Core.Tests/Infrastructure/Verification/CompareVerificationUnitTests.cs
+++ src/IDI.Core.Tests/Infrastructure/Verification/CompareVerificationUnitTests.cs
@@ -29,6 +29,27 @@ namespace IDI.Core.Tests.Infrastructure
 
             public DateTime EndTime { get; set; }
         }
+        public class LessThanOrEqualToModel : Command
+        {
+            [Compare(CompareMethod.LessThanOrEqualTo, "EndTime")]
+            public DateTime StartTime { get; set; }
+
+            public DateTime EndTime { get; set; }
+        }
+        public class GreaterThanModel : Command
+        {
+            [Compare(CompareMethod.GreaterThan, "EndTime")]
+            public DateTime StartTime { get; set; }
+
+            public DateTime EndTime { get; set; }
+        }
+        public class GreaterThanOrEqualToModel : Command
+        {
+            [Compare(CompareMethod.GreaterThanOrEqualTo, "EndTime")]
+            public DateTime StartTime { get; set; }
+
+            public DateTime EndTime { get; set; }
+        }
         #endregion
 
         private DateTime current = DateTime.Now;
@@ -91,6 +112,101 @@ namespace IDI.Core.Tests.Infrastructure
             Assert.IsFalse(result);
             Assert.AreEqual(1, errors.Count);
             Assert.AreEqual("'StartTime'应小于'EndTime'.", errors[0]);
+        }
+
+        [TestMethod]
+        public void It_Should_Be_Pass_When_StartTime_LessThanOrEqualTo_EndTime()
+        {
+            var model = new LessThanOrEqualToModel { StartTime = current.Date.AddDays(-1), EndTime = current.Date };
+
+            List<string> errors;
+
+            var result = model.IsValid(out errors);
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(0, errors.Count);
+
+            model = new LessThanOrEqualToModel { StartTime = current.Date, EndTime = current.Date };
+
+            result = model.IsValid(out errors);
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(0, errors.Count);
+        }
+
+        [TestMethod]
+        public void It_Should_Be_Not_Pass_When_StartTime_NotLessThanOrEqualTo_EndTime()
+        {
+            var model = new LessThanOrEqualToModel { StartTime = current.Date, EndTime = current.Date.AddDays(-1) };
+
+            List<string> errors;
+
+            var result = model.IsValid(out errors);
+
+            Assert.IsFalse(result);
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual("'StartTime'应小于或等于'EndTime'.", errors[0]);
+        }
+
+        [TestMethod]
+        public void It_Should_Be_Pass_When_StartTime_GreaterThan_EndTime()
+        {
+            var model = new GreaterThanModel { StartTime = current.Date, EndTime = current.Date.AddDays(-1) };
+
+            List<string> errors;
+
+            var result = model.IsValid(out errors);
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(0, errors.Count);
+        }
+
+        [TestMethod]
+        public void It_Should_Be_Not_Pass_When_StartTime_NotGreaterThan_EndTime()
+        {
+            var model = new GreaterThanModel { StartTime = current.Date, EndTime = current.Date.AddDays(1) };
+
+            List<string> errors;
+
+            var result = model.IsValid(out errors);
+
+            Assert.IsFalse(result);
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual("'StartTime'应大于'EndTime'.", errors[0]);
+        }
+
+        [TestMethod]
+        public void It_Should_Be_Pass_When_StartTime_GreaterThanOrEqualTo_EndTime()
+        {
+            var model = new GreaterThanOrEqualToModel { StartTime = current.Date, EndTime = current.Date.AddDays(-1) };
+
+            List<string> errors;
+
+            var result = model.IsValid(out errors);
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(0, errors.Count);
+
+            model = new GreaterThanOrEqualToModel { StartTime = current.Date, EndTime = current.Date };
+
+            result = model.IsValid(out errors);
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(0, errors.Count);
+        }
+
+        [TestMethod]
+        public void It_Should_Be_Not_Pass_When_StartTime_NotGreaterThanOrEqualTo_EndTime()
+        {
+            var model = new GreaterThanOrEqualToModel { StartTime = current.Date.AddDays(-1), EndTime = current.Date };
+
+            List<string> errors;
+
+            var result = model.IsValid(out errors);
+
+            Assert.IsFalse(result);
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual("'StartTime'应大于或等于'EndTime'.", errors[0]);
         }
     }
 }
